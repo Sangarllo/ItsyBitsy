cf4bfbdef9ac51b9c428749e3910461ad67cce56
Santiago Garcia Llorente <santiago.garcia.llorente@gmail.com>
2019-10-09 21:05:15 +0200

    First Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA9NqvPj1jAfGoPHFu8mXlY_7nRKb01bBg',
+    authDomain: 'itsybitsy-2020.firebaseapp.com',
+    databaseURL: 'https://itsybitsy-2020.firebaseio.com',
+    projectId: 'itsybitsy-2020',
+    storageBucket: '',
+    messagingSenderId: '803914721271',
+    appId: '1:803914721271:web:dc7977531c43e0a5dcb0f2',
+    measurementId: 'G-97SSSWDTHR'
+  }
 };
 
 /*
