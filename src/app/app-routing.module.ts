0ac5f46e71c6248c27e690779af917993c406194
Santiago Garcia Llorente <santiago.garcia.llorente@gmail.com>
2019-10-09 22:02:23 +0200

    Add Google OAuth with @angular/fire

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AuthGuard } from './services/auth.guard';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'user-profile', component: UserProfileComponent,  canActivate: [AuthGuard] },
+  { path: 'user-profile', component: UserProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
