1854e5b776056f7a950f8070e14b0204631e6d6a
Santiago Garcia Llorente <santiago.garcia.llorente@gmail.com>
2019-10-10 23:42:57 +0200

    Add configuration for prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA9NqvPj1jAfGoPHFu8mXlY_7nRKb01bBg',
+    authDomain: 'itsybitsy-2020.firebaseapp.com',
+    databaseURL: 'https://itsybitsy-2020.firebaseio.com',
+    projectId: 'itsybitsy-2020',
+    storageBucket: '',
+    messagingSenderId: '803914721271',
+    appId: '1:803914721271:web:dc7977531c43e0a5dcb0f2',
+    measurementId: 'G-97SSSWDTHR'
+  }
 };
