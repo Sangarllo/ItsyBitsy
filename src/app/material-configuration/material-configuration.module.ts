36368f3f81e91839caa66fff60a6f154fe722914
Santiago Garcia Llorente <santiago.garcia.llorente@gmail.com>
2019-10-22 23:44:36 +0200

    Improve style in form items

--- src/app/material-configuration/material-configuration.module.ts
+++ src/app/material-configuration/material-configuration.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule, MatIconModule } from '@angular/material';
 
-const matModules = [MatCardModule, MatButtonModule];
+const matModules = [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule ];
 
 @NgModule({
   imports: [...matModules],
--- src/app/students/students-routing.module.ts
+++ src/app/students/students-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     component: StudentListComponent
   },
   {
-    path: 'new',
+    path: 'nuevo',
     component: StudentNewComponent
   },
   {
